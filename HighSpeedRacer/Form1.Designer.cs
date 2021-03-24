91bb32f5c7e331b49d6b79eb84e6277ce2985fc7
Jacob Boyd <jacoboyd611@ed.amdsb.ca>
2021-03-24 19:18:59 -0400

    jwewfn

--- HighSpeedRacer/Form1.Designer.cs
+++ HighSpeedRacer/Form1.Designer.cs
@@ -29,14 +29,32 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.insturctionLabel = new System.Windows.Forms.Label();
+            this.countDown = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
-            // timer1
+            // gameTimer
             // 
-            this.timer1.Enabled = true;
-            this.timer1.Interval = 20;
-            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // insturctionLabel
+            // 
+            this.insturctionLabel.AutoSize = true;
+            this.insturctionLabel.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.insturctionLabel.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.insturctionLabel.Location = new System.Drawing.Point(250, 131);
+            this.insturctionLabel.Name = "insturctionLabel";
+            this.insturctionLabel.Size = new System.Drawing.Size(378, 33);
+            this.insturctionLabel.TabIndex = 0;
+            this.insturctionLabel.Text = "Press space to start or escape to quit";
+            // 
+            // countDown
+            // 
+            this.countDown.Interval = 1000;
+            this.countDown.Tick += new System.EventHandler(this.countDown_Tick);
             // 
             // Form1
             // 
@@ -44,17 +62,22 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Green;
             this.ClientSize = new System.Drawing.Size(884, 548);
+            this.Controls.Add(this.insturctionLabel);
             this.DoubleBuffered = true;
             this.Name = "Form1";
             this.Text = "High Speed Racer";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label insturctionLabel;
+        private System.Windows.Forms.Timer countDown;
     }
 }
 
--- HighSpeedRacer/Form1.cs
+++ HighSpeedRacer/Form1.cs
@@ -18,17 +18,39 @@ namespace HighSpeedRacer
             InitializeComponent();
         }
 
-        #region Variables      
+        #region Variables   
         int dashSpace = 0;
 
         int dashesWidth = 10;
         int dashesHeight = 30;
+
+        int carX = 360;
+        int carSpeed = 20;
+
+        int centerLane = 360;
+        int leftLane = 150;
+        int rightLane = 575;
+
+        int closestLane = 0;
+
+        int counter = 0;
+        bool running = false;
+
+        bool rightDown = false;
+        bool leftDown = false;
 
         List<int> rightDashesY = new List<int>();
         List<int> leftDashesY = new List<int>();
 
         Pen blackPen = new Pen(Color.Black, 50);
         Pen whitePen = new Pen(Color.White, 10);
+
+
+        Font drawFont = new Font("Seoge Print", 40, FontStyle.Bold | FontStyle.Italic);
+        SolidBrush yellowBrush = new SolidBrush(Color.Yellow);
+        SolidBrush purpleBrush = new SolidBrush(Color.Purple);
+        SolidBrush orangeBrush = new SolidBrush(Color.OrangeRed);
+        SolidBrush greenBrush = new SolidBrush(Color.Green);
 
         SolidBrush blackBrush = new SolidBrush(Color.Black);
         SolidBrush whiteBrush = new SolidBrush(Color.White);
@@ -38,7 +60,7 @@ namespace HighSpeedRacer
 
         private void Timer1_Tick(object sender, EventArgs e)
         {
-
+            #region Move dashes
             if (rightDashesY.Count == 0 || dashSpace == 20)
             {
                 rightDashesY.Add(150);
@@ -57,8 +79,77 @@ namespace HighSpeedRacer
                 leftDashesY[i] += 5;
                 if (leftDashesY[i] > this.Height + 100) { leftDashesY.RemoveAt(i); }
             }
+            #endregion
+
+            #region Move player
+            if (running)
+            {
+                if (rightDown)
+                {
+                    carX += carSpeed;
+                    if (carX >= closestLane)
+                    {
+                        rightDown = false;
+                    }
+                }
+
+                if (leftDown)
+                {
+                    carX -= carSpeed;
+                    if (carX <= closestLane)
+                    {
+                        leftDown = false;
+                    }
+                }
+            }
+
+            #endregion
+
             dashSpace++;
             Refresh();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Right:
+                    if (running)
+                    {
+                        //decide what lane to end in
+                        if (carX < centerLane) { closestLane = centerLane; }
+                        else { closestLane = rightLane; }
+                        rightDown = true;
+
+                        //prevents from moving outside the lanes
+                        if (carX > rightLane) { rightDown = false; }
+                    }
+                    break;
+                    
+
+                case Keys.Left:
+                    if (running)
+                    {
+                        //decide what lane to end in
+                        if (carX > centerLane) { closestLane = centerLane; }
+                        else { closestLane = leftLane; }
+                        leftDown = true;
+
+                        //prevents from moving outside the lanes
+                        if (carX < leftLane) { leftDown = false; }
+                    }
+                    break;
+
+                case Keys.Space:
+                    //running = true;
+                    if (running == false)
+                    {
+                        countDown.Enabled = true;
+                        insturctionLabel.Visible = false;
+                    }
+                    break;
+            }
+
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
@@ -78,9 +169,7 @@ namespace HighSpeedRacer
             e.Graphics.FillRectangle(blackBrush, 250, 200, 350, this.Height);
 
             e.Graphics.RotateTransform(-10);
-            #endregion
 
-            #region
             for (int i = 0; i < rightDashesY.Count; i++)
             {
                 e.Graphics.FillRectangle(whiteBrush, 450, rightDashesY[i], dashesWidth, dashesHeight);
@@ -97,7 +186,49 @@ namespace HighSpeedRacer
             e.Graphics.FillRectangle(blueBrush, 0, 0, this.Width, 200);
 
             //car
-            e.Graphics.DrawImage(Properties.Resources.car, 50, 20, 200, 400);
+            e.Graphics.DrawImage(Properties.Resources.car, carX, 460, 150, 75);
+
+            if (running)
+            {
+                //print obstacles
+            }
+            else
+            {
+                #region Title
+                int startingPoint = 275;
+                e.Graphics.DrawString("S", drawFont, purpleBrush, startingPoint, 60);
+                e.Graphics.DrawString("P", drawFont, orangeBrush, startingPoint + 25, 60);
+                e.Graphics.DrawString("E", drawFont, yellowBrush, startingPoint + 50, 60);
+                e.Graphics.DrawString("E", drawFont, yellowBrush, startingPoint + 75, 60);
+                e.Graphics.DrawString("D", drawFont, purpleBrush, startingPoint + 100, 60);
+
+                e.Graphics.DrawString("R", drawFont, orangeBrush, startingPoint + 150, 60);
+                e.Graphics.DrawString("A", drawFont, yellowBrush, startingPoint + 175, 60);
+                e.Graphics.DrawString("C", drawFont, purpleBrush, startingPoint + 200, 60);
+                e.Graphics.DrawString("E", drawFont, yellowBrush, startingPoint + 230, 60);
+                e.Graphics.DrawString("R", drawFont, orangeBrush, startingPoint + 255, 60);
+                e.Graphics.DrawString("S", drawFont, purpleBrush, startingPoint + 280, 60);               
+                #endregion
+            }
+
+            if (counter == 1) { e.Graphics.DrawString("3", drawFont, purpleBrush, 400, 300); }
+            else if (counter == 2) { e.Graphics.DrawString("2", drawFont, orangeBrush, 400, 300); }
+            else if (counter == 3) { e.Graphics.DrawString("1", drawFont, yellowBrush, 400, 300); }
+            else if (counter == 4) 
+            { 
+                e.Graphics.DrawString("GO", drawFont, greenBrush, 375, 300);
+                running = true;
+            }
+            else if (counter == 5)
+            {
+                countDown.Enabled = false;
+                counter = 0;
+            }
+        }
+
+        private void countDown_Tick(object sender, EventArgs e)
+        {
+            counter++;
         }
     }
 }
